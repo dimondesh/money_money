4a6998851176bf9a06eb34274760e4954f4c3d5a
Adriana <adrianat9099@gmail.com>
2025-04-29 00:16:07 +0300

    change

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,30 +1,32 @@
-import { NavLink } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
-import styles from './Navigation.module.css';
-import { FaHome, FaChartBar, FaDollarSign } from 'react-icons/fa';
+import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import styles from "./Navigation.module.css";
+import { FaHome, FaChartBar, FaDollarSign } from "react-icons/fa";
 
 const Navigation = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
-  const isTabletOrDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  const isTabletOrDesktop = useMediaQuery({ query: "(min-width: 768px)" });
 
   const getNavLinkClass = ({ isActive }) =>
     isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink;
 
   return (
     <nav className={styles.navWrapper}>
-      <NavLink to="/dashboard/home" className={getNavLinkClass}>
-         <FaHome className={styles.navIcon}/>
-         {isTabletOrDesktop && <span className={styles.navText}>Home</span>}
-         {isMobile && <span className={styles.navText}>Home</span>}
+      <NavLink to="/" className={getNavLinkClass}>
+        <FaHome className={styles.navIcon} />
+        {isTabletOrDesktop && <span className={styles.navText}>Home</span>}
+        {isMobile && <span className={styles.navText}>Home</span>}
       </NavLink>
-      <NavLink to="/dashboard/statistics" className={getNavLinkClass}>
-         <FaChartBar className={styles.navIcon}/>
-         {isTabletOrDesktop && <span className={styles.navText}>Statistics</span>}
-         {isMobile && <span className={styles.navText}>Statistics</span>}
+      <NavLink to="/statistics" className={getNavLinkClass}>
+        <FaChartBar className={styles.navIcon} />
+        {isTabletOrDesktop && (
+          <span className={styles.navText}>Statistics</span>
+        )}
+        {isMobile && <span className={styles.navText}>Statistics</span>}
       </NavLink>
       {isMobile && (
-        <NavLink to="/dashboard/currency" className={getNavLinkClass}>
-          <FaDollarSign className={styles.navIcon}/>
+        <NavLink to="/currency" className={getNavLinkClass}>
+          <FaDollarSign className={styles.navIcon} />
           <span className={styles.navText}>Currency</span>
         </NavLink>
       )}
