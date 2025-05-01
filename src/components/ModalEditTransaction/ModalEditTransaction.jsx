062ff4c1a247d9235dc2634ea7a138623daf649f
Natalia Kurthan <kurthannatalia@gmail.com>
2025-05-02 02:20:57 +0300

    Fix: Update ModalEditTransaction to support transaction form editing

--- src/components/ModalEditTransaction/ModalEditTransaction.jsx
+++ src/components/ModalEditTransaction/ModalEditTransaction.jsx
@@ -1,4 +1,6 @@
 import EditTransactionForm from '../EditTransactionForm/EditTransactionForm';
+import ModalWrapper from '../ModalWrapper/ModalWrapper';
+import { selectIsEditModalOpen } from '../../redux/modals/selectors';
 import { useSelector } from 'react-redux';
 
 const ModalEditTransaction = () => {
