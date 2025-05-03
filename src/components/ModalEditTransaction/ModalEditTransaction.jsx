86845a08236ca2d2204cf82b1b89d3d041f33fa0
Natalia Kurthan <kurthannatalia@gmail.com>
2025-05-04 01:58:09 +0300

    refactor: pass transactionId from modal state to EditTransactionForm

--- src/components/ModalEditTransaction/ModalEditTransaction.jsx
+++ src/components/ModalEditTransaction/ModalEditTransaction.jsx
@@ -1,14 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import EditTransactionForm from "../EditTransactionForm/EditTransactionForm";
-// import { selectIsEditModalOpen } from "@redux/modals/selectors";
-import { useSelector } from "react-redux";
 import Modal from "components/Modal/Modal";
-import { closeModalEditTransaction } from "@redux/modal/modalSlice";
+import { closeModalEditTransaction } from "../../redux/modal/modalSlice";
 import { createPortal } from "react-dom";
+import { selectIsEditModalOpen } from "../../redux/modal/selectors";
 import { useEffect } from "react";
 
+// import { selectIsEditModalOpen } from "@redux/modals/selectors"; // можливо, ця частина потрібна для доступу до стейту
 const modalRoot = document.getElementById("modal-root");
-
 const ModalEditTransaction = () => {
+  const dispatch = useDispatch(); // Ініціалізуємо dispatch
+  const isOpen = useSelector(selectIsEditModalOpen); // Вибираємо стан modal з Redux (це потрібно для перевірки чи відкритий модал)
+
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.code === "Escape") {
@@ -44,4 +48,5 @@ const ModalEditTransaction = () => {
     modalRoot
   );
 };
+
 export default ModalEditTransaction;
