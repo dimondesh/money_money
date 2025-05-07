f4733619ae5ca1014f2252482f7910603e755793
dimondesh <dimondesh1337@gmail.com>
2025-05-07 15:56:57 +0300

    changed site backend url

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const walletAPI = axios.create({
-  baseURL: "https://money-money-backend.onrender.com",
+  baseURL: "https://money-money-backend-k4hn.onrender.com",
   withCredentials: true,
 });
 
